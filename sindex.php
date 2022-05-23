d594191b6ff31635b2ab13511c79dcd84b2a38ba
eyestrain <prodigalp@gmail.com>
2022-05-23 10:49:43 +0800

    Added boiler plate

---
+++ sindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boiler Plate 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum nemo architecto magnam? Eligendi eius quaerat fuga cum, enim dolor assumenda eveniet. Ad nostrum quasi aut. Ex alias at illum nesciunt.
+    Iusto minus ea eligendi, totam porro pariatur sunt, magnam alias ex nihil beatae animi quae voluptatum nobis ipsum asperiores dolore perferendis tenetur? Labore explicabo repellat nesciunt qui ea quas adipisci.</p>    
+    <h1>Boiler Plate 1</h1>
+</body>
+</html>
